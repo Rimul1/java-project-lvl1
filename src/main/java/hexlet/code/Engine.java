75d299a48c4fb61ac8eba897884792f97ee0fbfb
Rimul1 <riyazmullin3@gmail.com>
2022-06-22 22:55:44 +0300

    fix bags linter p.3

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -10,49 +10,52 @@ import java.util.Scanner;
 
 public class Engine {
     public static void print() {
-        System.out.println("""
-                Please enter the game number and press Enter.
-                1 - Greet
-                2 - Even
-                3 - Calc
-                4 - GCD
-                5 - Progression
-                6 - Prime
-                0 - Exit""");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Your choice: ");
-        int number = scanner.nextInt();
-        System.out.println();
-        final int one = 1;
-        final int two = 2;
-        final int three = 3;
-        final int four = 4;
-        final int five = 5;
-        final int six = 6;
-        final int zero = 0;
-        switch (number) {
-            case zero:
-                break;
-            case one:
-                Cli cli = new Cli();
-                break;
-            case two:
-                Even.parity();
-                break;
-            case three:
-                Calculator.operations();
-                break;
-            case four:
-                GreatestCommonDivisor.gcd();
-                break;
-            case five:
-                Progression.arithmetic();
-                break;
-            case six:
-                PrimeNumbers.prime();
-                break;
-            default:
-                break;
-        }
+        int number;
+        do {
+            System.out.println("""
+                    Please enter the game number and press Enter.
+                    1 - Greet
+                    2 - Even
+                    3 - Calc
+                    4 - GCD
+                    5 - Progression
+                    6 - Prime
+                    0 - Exit""");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Your choice: ");
+            number = scanner.nextInt();
+            System.out.println();
+            final int one = 1;
+            final int two = 2;
+            final int three = 3;
+            final int four = 4;
+            final int five = 5;
+            final int six = 6;
+            final int zero = 0;
+            switch (number) {
+                case zero:
+                    break;
+                case one:
+                    Cli cli = new Cli();
+                    break;
+                case two:
+                    Even.parity();
+                    break;
+                case three:
+                    Calculator.operations();
+                    break;
+                case four:
+                    GreatestCommonDivisor.gcd();
+                    break;
+                case five:
+                    Progression.arithmetic();
+                    break;
+                case six:
+                    PrimeNumbers.prime();
+                    break;
+                default:
+                    break;
+            }
+        } while (number != 0);
     }
 }
